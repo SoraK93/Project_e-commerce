7480e4662334b396f08ecc6da15b6b9691345bad
SoraK93 <SoraK93@users.noreply.github.com>
2026-01-27 17:33:39 +0530

    Add all routes middleware to server

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+// import packages
 const express = require("express");
 const dotenv = require("dotenv");
+
+// import main api route
+const productRouter = require("./router/api");
+const orderRouter = require("./router/orders");
+const customerRouter = require("./router/customers");
+const cartRouter = require("./router/cart");
 
 dotenv.config({ quiet: true });
 
 const app = express();
 
 const port = process.env.PORT;
+
+// route middleware
+app.use("/product", productRouter);
+app.use("/order", orderRouter);
+app.use("/customer", customerRouter);
+app.use("/cart", cartRouter);
 
 app.listen(port, (err) => {
   if (err) {
