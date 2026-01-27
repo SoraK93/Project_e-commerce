777f6b07ef5fbac1182818ccc6f1c76f630790e0
SoraK93 <SoraK93@users.noreply.github.com>
2026-01-27 17:30:02 +0530

    database config setup

---
+++ model/database.js
@@ -0,0 +1,14 @@
+require("dotenv/config")
+const { Pool } = require("pg")
+
+const pool = new Pool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DB_NAME,
+  password: process.env.PASSWORD,
+  // Using default for development, need to change to a more secure number
+  port: process.env.DB_PORT
+});
+
+
+module.exports = pool
