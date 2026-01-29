1d80ff2125595aef96196596b185b4351014be6b
SoraK93 <SoraK93@users.noreply.github.com>
2026-01-29 17:27:56 +0530

    Update env variable

--- model/database.js
+++ model/database.js
@@ -3,9 +3,9 @@ const { Pool } = require("pg")
 
 const pool = new Pool({
   host: process.env.HOST,
-  user: process.env.USER,
+  user: process.env.DB_USER,
   database: process.env.DB_NAME,
-  password: process.env.PASSWORD,
+  password: process.env.DB_PASSWORD,
   // Using default for development, need to change to a more secure number
   port: process.env.DB_PORT
 });
