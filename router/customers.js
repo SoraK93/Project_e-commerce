23021c6c5c0f1a76abad9f19b4b18e2a732ef2af
SoraK93 <SoraK93@users.noreply.github.com>
2026-02-01 18:51:00 +0530

    Add customer delete request and update get base route

--- router/customers.js
+++ router/customers.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const pool = require("../model/database");
+const queryReturnError = require("../utility/api-errors");
 
 const customer = express.Router();
 
 customer.get("/", async (req, res, next) => {
-  const result = await pool.query("SELECT * FROM customers_details");
+  const result = await pool.query(
+    "SELECT name, phone, email, address FROM customers_details",
+  );
 
   if (result.rowCount === 0) {
-    const err = new Error("No registered customers found.");
-    err.status = 404
-    return next(err)
+    return next(queryReturnError("No registered customers found.", 404));
   }
 
-  res.send(result.rows);
+  res.status(200).json(result.rows);
 });
 
 customer.get("/:customerId", (req, res, next) => {
@@ -27,8 +28,19 @@ customer.patch("/", (req, res, next) => {
   res.send();
 });
 
-customer.delete("/:customerId", (req, res, next) => {
-  res.send();
+customer.delete("/:customerId", async (req, res, next) => {
+  const customerId = req.params.customerId;
+
+  const result = await pool.query(
+    "DELETE FROM customers_details WHERE id = $1 RETURNING *",
+    [customerId],
+  );
+
+  if (result.rowCount === 0) {
+    return next(queryReturnError("Invalid Id. Customer not found.", 404));
+  }
+
+  res.status().json();
 });
 
 module.exports = customer;
