17ea825f7884ec8f90fa004914b42ce9de9b5d34
SoraK93 <SoraK93@users.noreply.github.com>
2026-02-02 20:54:29 +0530

    update product & customer route, seperate controller logic

--- router/customers.js
+++ router/customers.js
@@ -1,69 +1,22 @@
 const express = require("express");
-const pool = require("../model/database");
-const queryReturnError = require("../utility/api-errors");
-const genPatchRouteQueryList = require("../utility/api-helper");
+const {
+  getCustomers,
+  getCustomerById,
+  createNewCustomer,
+  updateCustomerById,
+  deleteCustomerById,
+} = require("../controller/customerController");
 
 const customer = express.Router();
 
-customer.get("/", async (req, res, next) => {
-  const result = await pool.query(
-    "SELECT name, phone, email, address FROM customers_details",
-  );
+customer.get("/", getCustomers);
 
-  if (result.rowCount === 0) {
-    return next(queryReturnError("No registered customers found.", 404));
-  }
+customer.get("/:customerId", getCustomerById);
 
-  res.status(200).json(result.rows);
-});
+// customer.post("/", createNewCustomer);
 
-customer.get("/:customerId", async (req, res, next) => {
-  const customerId = req.params.customerId;
+customer.patch("/:customerId", updateCustomerById);
 
-  const result = await pool.query(
-    "SELECT name, phone, email, address FROM customers_details WHERE id = $1",
-    [customerId],
-  );
-
-  if (result.rowCount === 0) {
-    return next(queryReturnError("Invalid request.", 400));
-  }
-
-  res.status(200).json(result.rows[0]);
-});
-
-customer.post("/", (req, res, next) => {
-  // const { name, phone, email, password, address } = req.body;
-
-  res.send();
-});
-
-customer.patch("/:customerId", async (req, res, next) => {
-  const customerId = req.params.customerId;
-
-  const [updateFields, updateValues] = genPatchRouteQueryList(req.body, next);
-
-  const result = await pool.query(
-    `UPDATE customers_details SET ${updateFields.join(", ")} WHERE id = $${updateValues.length + 1}`,
-    [...updateValues, customerId],
-  );
-
-  res.status(204);
-});
-
-customer.delete("/:customerId", async (req, res, next) => {
-  const customerId = req.params.customerId;
-
-  const result = await pool.query(
-    "DELETE FROM customers_details WHERE id = $1 RETURNING name, phone, email, address",
-    [customerId],
-  );
-
-  if (result.rowCount === 0) {
-    return next(queryReturnError("Invalid Id. Customer not found.", 404));
-  }
-
-  res.status(200).json(result.rows[0]);
-});
+customer.delete("/:customerId", deleteCustomerById);
 
 module.exports = customer;
--- router/products.js
+++ router/products.js
@@ -1,68 +1,26 @@
 const express = require("express");
-const pool = require("../model/database");
-const { v4: uuidv4 } = require("uuid");
-const queryReturnError = require("../utility/api-errors");
-const { genPatchRouteQueryList } = require("../utility/api-helper");
+const {
+  getProducts,
+  getProductById,
+  createNewProduct,
+  updateProductById,
+  deleteProductById,
+} = require("../controller/productController");
 
 const product = express.Router();
 
 // requests all products in the database
-product.get("/", async (req, res, next) => {
-  const result = await pool.query("SELECT * FROM products");
-  res.send(result.rows);
-});
+product.get("/", getProducts);
 
 // request for a specific product in the database
-product.get("/:productId", async (req, res, next) => {
-  const productId = req.params.productId;
-
-  const result = await pool.query("SELECT * FROM products WHERE id = $1", [
-    productId,
-  ]);
-
-  res.status(200).json(result.rows[0]);
-});
+product.get("/:productId", getProductById);
 
 // creates a new product
-product.post("/", async (req, res, next) => {
-  const { name, description, in_stock, price } = req.body;
-  const id = uuidv4();
-  const seller_id = uuidv4();
-
-  const result = await pool.query(
-    "INSERT INTO products VALUES ($1, $2, $3, $4, $5, $6)",
-    [id, name, description, in_stock, price, seller_id],
-  );
-  res.status(201).json({ message: "create successful" });
-});
+product.post("/", createNewProduct);
 
 // request update for a specific product in database
-product.patch("/:productId", async (req, res, next) => {
-  const productId = req.params.productId;
+product.patch("/:productId", updateProductById);
 
-  const [updateFields, updateValues] = genPatchRouteQueryList(req.body, next);
-
-  const result = await pool.query(
-    `UPDATE products SET ${updateFields.join(", ")} WHERE id = $${updateValues.length + 1}`,
-    [...updateValues, productId],
-  );
-
-  res.status(204).json({ message: "update successful" });
-});
-
-product.delete("/:productId", async (req, res, next) => {
-  const productId = req.params.productId;
-
-  const result = await pool.query(
-    "DELETE FROM products WHERE id = $1 RETURNING *",
-    [productId],
-  );
-
-  if (result.rowCount === 0) {
-    return next(queryReturnError("Product not found", 404));
-  }
-
-  res.status(200).send(result);
-});
+product.delete("/:productId", deleteProductById);
 
 module.exports = product;
