7775ee277ff2972ed5110a5a09813c5341c9e170
SoraK93 <SoraK93@users.noreply.github.com>
2026-02-02 16:12:46 +0530

    customer route add get & patch, update delete

--- router/customers.js
+++ router/customers.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const pool = require("../model/database");
 const queryReturnError = require("../utility/api-errors");
+const genPatchRouteQueryList = require("../utility/api-helper");
 
 const customer = express.Router();
 
@@ -16,23 +17,45 @@ customer.get("/", async (req, res, next) => {
   res.status(200).json(result.rows);
 });
 
-customer.get("/:customerId", (req, res, next) => {
-  res.send();
+customer.get("/:customerId", async (req, res, next) => {
+  const customerId = req.params.customerId;
+
+  const result = await pool.query(
+    "SELECT name, phone, email, address FROM customers_details WHERE id = $1",
+    [customerId],
+  );
+
+  if (result.rowCount === 0) {
+    return next(queryReturnError("Invalid request.", 400));
+  }
+
+  res.status(200).json(result.rows[0]);
 });
 
 customer.post("/", (req, res, next) => {
+  // const { name, phone, email, password, address } = req.body;
+
   res.send();
 });
 
-customer.patch("/", (req, res, next) => {
-  res.send();
+customer.patch("/:customerId", async (req, res, next) => {
+  const customerId = req.params.customerId;
+
+  const [updateFields, updateValues] = genPatchRouteQueryList(req.body, next);
+
+  const result = await pool.query(
+    `UPDATE customers_details SET ${updateFields.join(", ")} WHERE id = $${updateValues.length + 1}`,
+    [...updateValues, customerId],
+  );
+
+  res.status(204);
 });
 
 customer.delete("/:customerId", async (req, res, next) => {
   const customerId = req.params.customerId;
 
   const result = await pool.query(
-    "DELETE FROM customers_details WHERE id = $1 RETURNING *",
+    "DELETE FROM customers_details WHERE id = $1 RETURNING name, phone, email, address",
     [customerId],
   );
 
@@ -40,7 +63,7 @@ customer.delete("/:customerId", async (req, res, next) => {
     return next(queryReturnError("Invalid Id. Customer not found.", 404));
   }
 
-  res.status().json();
+  res.status(200).json(result.rows[0]);
 });
 
 module.exports = customer;
