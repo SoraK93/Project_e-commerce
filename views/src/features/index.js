9e6a8689f5eaee1f325ea820bf0602223f6534a8
SoraK93 <SoraK93@users.noreply.github.com>
2026-02-09 14:44:43 +0530

    streamlines all export in feature folder
    this will remove repeated occurance of "../../" when importing a deeply nested file.

--- views/src/features/index.js
+++ views/src/features/index.js
@@ -1,4 +1,19 @@
 import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
+import { ErrorPage } from "./ErrorPage/error";
 
-export { Footer, Header };
+import productReducer, { selectProduct } from "./Products/productsSlice";
+import { AllProducts } from "./Products/Products";
+import { getAllProduct, getProductById } from "./Products/productsAPI";
+import { ProductById } from "./Products/ProductById";
+const products = {
+  component: { AllProducts, ProductById },
+  reducer: productReducer,
+  select: selectProduct,
+  api: {
+    getAllProduct,
+    getProductById
+  },
+};
+
+export { Footer, Header, products, ErrorPage };
