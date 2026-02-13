b2a0e66d63a1dcc545495e2ab57e4cb3184751d9
SoraK93 <SoraK93@users.noreply.github.com>
2026-02-13 10:05:06 +0530

    making imports more clean by refactoring it to seperate files

--- views/src/features/index.js
+++ views/src/features/index.js
@@ -1,25 +1,8 @@
 import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
 import { ErrorPage } from "./ErrorPage/error";
+import { products } from "./Products";
+import { auth } from "./LoginRegisterPage";
+import { users } from "./users";
 
-import productReducer, { selectProduct } from "./Products/productsSlice";
-import { AllProducts } from "./Products/Products";
-import { getAllProduct, getProductById } from "./Products/productsAPI";
-import { ProductById } from "./Products/ProductById";
-const products = {
-  component: { AllProducts, ProductById },
-  reducer: productReducer,
-  select: selectProduct,
-  api: {
-    getAllProduct,
-    getProductById,
-  },
-};
-
-import { Login } from "./LoginRegisterPage/Login";
-import { Register } from "./LoginRegisterPage/Register";
-const auth = {
-  component: { Login, Register },
-};
-
-export { Footer, Header, ErrorPage, products, auth };
+export { Footer, Header, ErrorPage, products, auth, users };
