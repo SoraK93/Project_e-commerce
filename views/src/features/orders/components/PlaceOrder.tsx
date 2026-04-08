57247a255e3c2696faddb560e9240894141d5da4
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-04-08 10:02:54 +0530

    update cart ui after order confirmation depending on order confirmation status, cart ui will be updated

--- views/src/features/orders/components/PlaceOrder.tsx
+++ views/src/features/orders/components/PlaceOrder.tsx
@@ -1,6 +1,19 @@
-import { type JSX } from "react";
+import { useEffect, type JSX } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectOrderCartStatus } from "../api/orderSlice";
+import { cartCleanUpOnOrder } from "@features/cart/api/cartSlice";
 
 const PlaceOrder = (): JSX.Element => {
+  // If successful empty cart, if failed dont touch it
+  const orderStatus = useSelector(selectOrderCartStatus)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (orderStatus == "fulfilled") {
+      dispatch(cartCleanUpOnOrder([]))
+    }
+  }, [])
+
   return (
     <div>
       <p>Confirmation message: Order placement successful/ failed.</p>
